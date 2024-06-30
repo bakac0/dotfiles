d4a9aff5b84881c0e72f202f11c7abd48a1ae62a
BAKAC0 <38254398+bakac0@users.noreply.github.com>
2024-06-30 14:56:24 +0300

    Update exports.zsh

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -7,13 +7,24 @@ export PATH="$PATH:/usr/local/go/bin"
 # history
 export HISTSIZE=100000
 export SAVEHIST=20000
+setopt hist_ignore_dups     # do not record an event that was just recorded again
+setopt hist_ignore_all_dups # delete an old recorded event if a new event is a duplicate
+setopt hist_ignore_space    # do not record an event starting with a space
+setopt hist_save_no_dups    # do not write a duplicate event to the history file
+setopt inc_append_history   # write to the history file immediately, not when the shell exits
+setopt share_history        # share history between terminals
 
 # fix vi plugin conflicting with fzf
 export ZVM_INIT_MODE=sourcing
 
 # FZF
-if [[ ! "$PATH" == */home/bakac0/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/bakac0/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  PATH="${PATH}:${HOME}/.fzf/bin"
+fi
+
+# Brew
+f [[ ! "$PATH" == */opt/homebrew/bin* ]]; then
+  PATH="${PATH}:/opt/homebrew/bin"
 fi
 
 # Werf
